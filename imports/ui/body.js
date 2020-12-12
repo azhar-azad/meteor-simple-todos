fb4fbc45a8b9d84eddc0185e0162b5bf452f2f89
azhar-azad <azhar.u.azad@gmail.com>
2020-12-12 17:11:08 +0600

    Temporary UI state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,44 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Todos } from "../api/todos";
 
 import './body.html';
 import './todo';
 
+Template.body.onCreated(function bodyOnCreated() {
+  /*
+  Adding a client-side data filtering feature in the app, so that the users
+  can check a box to only see incomplete tasks. For this ReactiveDict will be used.
+  Learn about ReactiveDict [ https://atmospherejs.com/meteor/reactive-dict ]
+  
+  ReactiveDice: ReactiveDict is a reactive data source like Mongo collections,
+  but is not synced with the server like collections are.
+  This makes a ReactiveDict a convenient place to store temporary UI state
+  like the checkbox (Hide Completed Tasks)
+  
+  Setting up a new ReactiveDict and attach it to the body template instance
+  (as this is where the checkbox's state will be stored) when it is first created.
+   */
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
+  // Return the todos from MongoDB to template (body.html)
   todos() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Todos.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Todos.find({}, { sort: { createdAt: -1 } });
+  },
+  
+  // Return the count of todos that are not completed yet
+  // i.e. those todos that have checked property set to true
+  incompleteCount() {
+    return Todos.find({ checked: { $ne: true } }).count();
   }
 });
 
@@ -32,5 +63,13 @@ Template.body.events({
     
     // Clear form
     event.target.todo.value = '';
+  },
+  
+  /*
+  Added an event handler to update the ReactiveDice variable
+  when the checkbox is checked or unchecked.
+   */
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   }
 });
